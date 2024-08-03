4f97e97a6fe79c3c56cda8e68330f61aea75ccd9
Athul Raj <43885340+outoftune2000@users.noreply.github.com>
2024-08-03 15:30:02 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,12 +4,10 @@ const { Routes } = require('discord-api-types/v9');
 require('dotenv').config();
 
 const TOKEN = process.env.BOT_TOKEN;
-const CLIENT_ID = process.env.CLIENT_ID; // Add your client ID here
-const GUILD_ID = process.env.GUILD_ID; // Optional: Add your guild ID for testing
-
+const CLIENT_ID = process.env.CLIENT_ID;
+//const GUILD_ID = process.env.GUILD_ID;
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// Define the slash command
 const commands = [
     {
         name: 'status',
@@ -25,7 +23,7 @@ const rest = new REST({ version: '9' }).setToken(TOKEN);
         console.log('Started refreshing application (/) commands.');
 
         await rest.put(
-            Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), // Use Routes.applicationCommands(CLIENT_ID) for global commands
+            Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), 
             { body: commands },
         );
 
